10b2fbd05e952d40460e79df52988b5c037d115d
oadebiyi <tobi.adebiyi@kpmg.co.uk>
2016-04-23 18:29:02 +0100

    Added Repository interface

---
+++ Tee.FamilyApp.DAL/Entities/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tee.FamilyApp.DAL.Entities
+{
+    internal interface IRepository<T> : IDisposable
+    {
+        IEnumerable<T> GetAll();
+
+        T Get(int Id);
+
+        int Add(T entity);
+
+        bool Delete(int id);
+
+        bool Update(T entity);
+    }
+}
